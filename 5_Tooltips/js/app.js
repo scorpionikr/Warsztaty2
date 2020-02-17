f0478ba793e00433dce69dd5368db50c19c95e26
Rafal Bartkowski <scorpionikr@gmail.com>
2020-02-17 17:24:37 +0100

    Zrobiony tooltips

--- 5_Tooltips/js/app.js
+++ 5_Tooltips/js/app.js
@@ -1,3 +1,27 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    const tooltips = document.querySelectorAll(".tooltip");
+
+// console.log(tooltips)
+
+
+    for (let i=0; i <tooltips.length; i++) {
+        tooltips[i].addEventListener("mouseover", function(){
+                let string = tooltips[i].dataset["text"]
+                const toadd = document.createElement("span");
+                toadd.className = "tooltipText"
+                toadd.innerText = string
+                this.appendChild(toadd);
+        });
+        tooltips[i].addEventListener("mouseout", function(){
+                const toDelete = this.firstElementChild;
+                toDelete.parentElement.removeChild(toDelete);
+                // console.log(toDelete)
+        });
+    }
+
+});
